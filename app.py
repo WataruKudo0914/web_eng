f4196b8afacb1f7ead0891e34dd20723cc441d1f
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-06-16 16:57:14 +0900

    商品のdbまで作った

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 from flask import Flask, render_template,request
 from werkzeug.utils import secure_filename
+#posgresqlへアクセスするモジュール
+import psycopg2
+import psycopg2.extras
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/web_eng'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+#データベースの構造を変えたら
+# flask db update　コマンドをうってマイグレーとする。
+#userのdb
+class User_table(db.Model):
+    id= db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(20),index=True,unique=True)
+    def __repr__(self):
+        return '<User %r>'%self.username
+
+class Goods_table(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    goods_name=db.Column(db.String(40))
+    rental_fee=db.Column(db.Integer)
+    description=db.Column(db.String(100))
+    def __repr__(self):
+        return '<User %r>'%self.username
+
+@app.route('/test')
+def register():
+    new_User=User_table(username='shumpei_kikuta')
+    db.session.add(new_User)
+    db.session.commit()
+    # username = 'shumpei kikuta'
+    return render_template('result.html',username = new_User)
+
 
 @app.route('/')
 def index():
@@ -15,18 +50,18 @@ def sign_up():
 def sign_in():
     return render_template("sign_in.html")
 
-@app.route("/top_page",methods=["POST"])
-def login_success():
-    if request.form['username'] and request.form['password']:
-        username = request.form['username']
-        return render_template("top_page.html",username=username)
-    else:
-        return render_template("error.html")
-
-@app.route("/top_page",methods=["GET"])
-def top_page():
-    username = "ゲスト"
-    return render_template("top_page.html",username=username)
+@app.route("/top_page",methods=["POST","GET"])
+def login():
+    goods = Goods_table.query.all()
+    if request.method =="POST":
+        if request.form['username'] and request.form['password']:
+            username = request.form['username']
+            return render_template("top_page.html",username=username,goods =goods)
+        else:
+            return render_template("error.html")
+    elif request.method== "GET":
+        username = "ゲスト"
+        return render_template("top_page.html",username=username,goods=goods)
 
 @app.route("/post_goods")
 def post_goods():
@@ -42,12 +77,17 @@ def complete_post_goods():
         goods_name = request.form['goods_name']
         rental_fee = request.form['rental_fee']
         description = request.form['description']
-
+        new_goods = Goods_table(goods_name=goods_name,rental_fee=rental_fee,description=description)
+        db.session.add(new_goods)
+        db.session.commit()
         return render_template("complete_post_goods.html",goods_name=goods_name,rental_fee=rental_fee,description=description)
     else:
         return render_template("error.html")
 
-
+#テーブルの初期化のコマンド、これをしないとSQLAlchemyがdbにアクセスできない。
+@app.cli.command('initdb')
+def initdb_command():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
