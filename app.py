10f3a9077a270ce7a3c98dd5747a9c1fe785cb31
Oyunka <oyunka.t@gmail.com>
2018-07-01 22:12:23 +0900

    home_page designing and css update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from flask import Flask, render_template,request
 from werkzeug.utils import secure_filename
 #posgresqlへアクセスするモジュール
@@ -7,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/web_eng'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
