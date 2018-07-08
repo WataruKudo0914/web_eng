1fc460df6f28ac8452773155ae73c7ed064af745
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-07-08 16:34:58 +0900

    貸している商品の表示

--- app.py
+++ app.py
@@ -235,9 +235,19 @@ def mypage():
     user_information = User_table.query.filter(User_table.id==id).first()
     posted_goods = Goods_table.query.filter(Goods_table.id==id).all()
     rental_goods_id = Deal_table.query.filter(Deal_table.borrower_id==id).all()
-    for i in rental_goods_id:
-        rental_goods = Goods_table.query.filter(Goods_table.goods_id == i.goods_id).all()
-    return render_template("mypage.html",user_information=user_information,posted_goods=posted_goods,rental_goods=rental_goods)
+    if len(rental_goods_id) != 0:
+        for i in rental_goods_id:
+            rental_goods = Goods_table.query.filter(Goods_table.goods_id == i.goods_id).all()
+    else:
+        rental_goods = []
+
+    lend_goods_id = Deal_table.query.filter(Deal_table.lender_id==id).all()
+    if len(lend_goods_id) != 0:
+        for i in lend_goods_id:
+            lend_goods = Goods_table.query.filter(Goods_table.goods_id == i.goods_id).all()
+    else:
+        lend_goods = []
+    return render_template("mypage.html",user_information=user_information,posted_goods=posted_goods,rental_goods=rental_goods,lend_goods=lend_goods)
 
 @app.route("/update_phase",methods=["POST"])
 def update_phase():
